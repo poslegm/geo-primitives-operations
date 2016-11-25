409c62ccd1868ae79d569026a35332f568115328
Mikhail Chugunkov <poslegm@gmail.com>
2016-11-25 18:23:43 +0300

    Added method for remove element from array

--- geo-utils.js
+++ geo-utils.js
@@ -6,6 +6,18 @@ Array.prototype.clear = function () {
   return this.splice(0, this.length);
 }
 
+/*
+ * Удаляет элемент из массива с уникальными объектами, сравнимыми через ===
+  * */
+Array.prototype.removeFromIndexesArray = function (x) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] === x) {
+      this.splice(i, 1);
+      return;
+    }
+  }
+}
+
 module.exports.addToListsDict = function (dict, key, value) {
   if (dict[key] === undefined) {
     dict[key] = [value];
